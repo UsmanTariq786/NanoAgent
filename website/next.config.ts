2218c0b815941cbf0c99e6371ce703a8eaefb200
usmantariq <usmantariq0896@gmail.com>
2026-08-25 16:08:21 +0500

    duplicate images public deleted

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- images: {
-   unoptimized: true,
- },
+  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
   outputFileTracingRoot: __dirname,
 };
 
